ff75d8b4502617e119b76f845d08f756465dbddb
JD Smith <93749+jdtsmith@users.noreply.github.com>
2022-09-14 17:19:35 -0400

    Relative filenames are also built-in

--- mode-minder.el
+++ mode-minder.el
@@ -41,7 +41,8 @@
 	 (tag (cond
 	       ((file-in-directory-p sfile package-user-dir)
 		"(P)")
-	       ((not (file-in-directory-p sfile mode-minder--builtin-dir))
+	       ((not (or (not (eq (aref sfile 0) ?/)) ;relative filename
+			 (file-in-directory-p sfile mode-minder--builtin-dir)))
 		"(O)")))
 	 (pad (max 0 (- mode-minder-pad (length mstr) (length tag)))))
     (princ mstr)
